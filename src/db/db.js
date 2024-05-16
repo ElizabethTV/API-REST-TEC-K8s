5dd4921d3bc38962cde04b7c82a3e6a21c3f03a2
ElizabethTV <lichardatovar@gmail.com>
2024-05-16 00:01:55 -0600

    first commit

--- src/db/db.js
+++ src/db/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
     user: 'postgres',
-    host: '192.168.1.7',
+    host: '192.168.1.12',
     database: 'Universidad',
     password: 'password',
     port: 5432
